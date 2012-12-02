9fdc8f2c5b4a4987d88d84ded47ec6bd1392904e
Bruno Paulin <brunopaulin@bpaulin.net>
2012-12-02 19:05:58 +0100

    modification d'organisation

--- src/Bpaulin/UpfitBundle/Controller/AjaxController.php
+++ src/Bpaulin/UpfitBundle/Controller/AjaxController.php
@@ -14,59 +14,5 @@ use Symfony\Component\HttpFoundation\Response;
 class AjaxController extends Controller
 {
 
-    /**
-     * @Route("/admin/users/list", name="admin_ajax_users_list")
-     * @Template()
-     */
-    public function usersListAction(Request $request)
-    {
-        $get = $request->query->all();
-        if (!isset($get['sEcho'])) {
-            $get['sEcho']=1;
-        }
-        /* Array of database columns which should be read and sent back to DataTables. Use a space where
-        * you want to insert a non-database field (for example a counter or static image)
-        */
-        $columns = array('id', 'username', 'email');
-        $get['columns'] = &$columns;
 
-        $em = $this->getDoctrine()->getEntityManager();
-        $ajaxTable = $em->getRepository('BpaulinUpfitBundle:User')->ajaxTable($get, true);
-
-        $countUnfiltered = $ajaxTable[0];
-        $rResult =  $ajaxTable[1]->getArrayResult();
-        /* Data set length after filtering */
-        $iFilteredTotal = count($rResult);
-
-        /*
-         * Output
-         */
-        $output = array(
-            "sEcho" => intval($get['sEcho']),
-            "iTotalRecords" => $em->getRepository('BpaulinUpfitBundle:User')->getCount(),
-            "iTotalDisplayRecords" => $countUnfiltered,
-            "aaData" => array()
-        );
-
-        foreach ($rResult as $aRow) {
-            $row = array();
-            for ($i=0; $i<count($columns); $i++) {
-                if ($columns[$i] == "email") {
-                    /* Special output formatting for 'version' column */
-                    $row[] = $aRow[ $columns[$i] ];
-                    $row[] = md5(strtolower(trim($aRow[ $columns[$i] ])));
-                } elseif ($columns[$i] != ' ') {
-                    /* General output */
-                    $row[] = $aRow[ $columns[$i] ];
-                }
-            }
-            $output['aaData'][] = $row;
-        }
-
-        unset($rResult);
-
-        return new Response(
-            json_encode($output)
-        );
-    }
 }
--- src/Bpaulin/UpfitBundle/Tests/Controller/AjaxControllerTest.php
+++ src/Bpaulin/UpfitBundle/Tests/Controller/AjaxControllerTest.php
@@ -11,7 +11,12 @@ class AjaxControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/ajax/admin/users/list');
+		$this->assertTrue($crawler->filter('html:contains("user1@upfit.com")')->count() > 0);
 
+        $crawler = $client->request('GET', '/ajax/admin/users/list?sSearch=user2&bSearchable_0=true');
+		$this->assertTrue($crawler->filter('html:contains("user1@upfit.com")')->count() == 0);
+
+        $crawler = $client->request('GET', '/ajax/admin/users/list?sSearch=user2');
         $this->assertTrue($crawler->filter('html:contains("user1@upfit.com")')->count() > 0);
     }
 }
