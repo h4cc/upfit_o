9e4d4e783e4076fc9ec492e6c9d8a66fe81570de
Bruno Paulin <brunopaulin@bpaulin.net>
2012-12-06 14:16:20 +0100

    ajout generation gravatar

--- src/Bpaulin/UpfitBundle/Entity/User.php
+++ src/Bpaulin/UpfitBundle/Entity/User.php
@@ -31,4 +31,23 @@ class User extends BaseUser
     {
         return $this->id;
     }
+
+    /**
+     * Get either a Gravatar URL or complete image tag for a specified email address.
+     *
+     * @param string $email The email address
+     * @param string $s     Size in pixels, defaults to 80px [ 1 - 2048 ]
+     * @param string $d     Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+     * @param string $r     Maximum rating (inclusive) [ g | pg | r | x ]
+     *
+     * @return String containing a URL
+     */
+    public function getGravatarUrl($s = 80, $d = 'mm', $r = 'g')
+    {
+        $url = 'http://www.gravatar.com/avatar/';
+        $url .= md5(strtolower(trim($this->getEmail())));
+        $url .= "?s=$s&d=$d&r=$r";
+
+        return $url;
+    }
 }
