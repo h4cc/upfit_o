b54fcb2a39d3d3934b09ec125ca69bcf3086cb41
Bruno <brunopaulin@bpaulin.net>
2012-12-08 16:35:09 +0100

    modif home

--- src/Bpaulin/UpfitBundle/Tests/FosUserTest.php
+++ src/Bpaulin/UpfitBundle/Tests/FosUserTest.php
@@ -13,10 +13,12 @@ class FosUserTest extends WebTestCase
         $crawler = $client->request('GET', '/login');
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 
-        $form = $crawler->selectButton('_submit')->form(array(
+        $form = $crawler->selectButton('_submit')->form(
+            array(
             '_username'       => 'user1',
             '_password'         => 'user1'
-        ));
+            )
+        );
         $client->submit($form);
         $crawler = $client->followRedirect();
         $this->assertEquals('http://localhost/', $client->getRequest()->getUri());
@@ -29,13 +31,14 @@ class FosUserTest extends WebTestCase
         $crawler = $client->request('GET', '/login');
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 
-        $form = $crawler->selectButton('_submit')->form(array(
+        $form = $crawler->selectButton('_submit')->form(
+            array(
             '_username'       => 'user1',
             '_password'         => 'qsdfqsdfqsdfqsdf'
-        ));
+            )
+        );
         $client->submit($form);
         $crawler = $client->followRedirect();
         $this->assertEquals('http://localhost/login', $client->getRequest()->getUri());
     }
-
 }
