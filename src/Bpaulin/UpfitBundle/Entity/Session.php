836392934f9e22559f8476f3530ab8c7ed9a3739
bpaulin <brunopaulin@bpaulin.net>
2013-03-09 11:56:17 +0100

    entity pour session

---
+++ src/Bpaulin/UpfitBundle/Entity/Session.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bpaulin\UpfitBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Session
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Bpaulin\UpfitBundle\Entity\SessionRepository")
+ */
+class Session
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
